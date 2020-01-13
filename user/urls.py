e918057101a2374f1caba0e767aec1a06e420a7a
unknown <rajasekhartechpy@gmail.com>
2020-01-13 17:08:27 +0530

    added logout

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import user_login, home ,user_register
+from .views import user_login, home ,user_register, user_logout
 urlpatterns = [
     path('home',home, name = "home"),
     path('', user_login, name = "user_login"),
     path('register/', user_register, name = "user_register"),
+    path('logout', user_logout, name ="user_logout"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render , redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate , logout
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
@@ -33,3 +33,6 @@ def user_register(request):
     else:
         form = UserCreationForm()
     return render(request, 'user/register.html', {'form': form})
+def user_logout(request):
+    logout(request)
+    return redirect('user_login')
